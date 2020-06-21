1cca5831118e77397a8a8f7c7f8549b11136c001
ShadyAgami <sagamy@road9media.com>
2020-06-22 01:57:41 +0200

    add routing to project

--- src/App.js
+++ src/App.js
@@ -1,12 +1,39 @@
 import React from 'react';
+import {Switch,Route} from "react-router-dom";
 
 import './App.css';
 import Homepage from "./pages/homepage";
 
+const HatsPage = (props) => {
+  console.log(props)
+  return (
+    <div>
+    <h1>Hats page</h1>
+  </div>
+  )
+
+}
+
+const HatPage = (props) => {
+  console.log(props)
+  return (
+    <div>
+    <h1> Hat {props.match.params.hatId} </h1>
+  </div>
+  )
+
+}
+
+
 function App() {
   return (
     <div>
-      <Homepage></Homepage>
+      <Switch>
+        <Route exact path='/' component={Homepage}/>
+        <Route  exact path='/hats' component={HatsPage}/>
+        <Route  path='/hats/:hatId' component={HatPage}/>
+      </Switch>
+      
     </div>
   );
 }
